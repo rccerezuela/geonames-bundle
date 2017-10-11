505925540c83442348e0095584f38b3b8875aa56
simeza <32630938+simeza@users.noreply.github.com>
2017-10-11 09:06:29 +0200

    Add continent

--- Entity/CountryRepository.php
+++ Entity/CountryRepository.php
@@ -58,6 +58,7 @@ class CountryRepository extends EntityRepository implements CountryRepositoryInt
         $postalCodeFormat = $source->getPostalCodeFormat();
         $postalCodeRegex  = $source->getPostalCodeRegex();
         $phonePrefix      = $source->getPhonePrefix();
+        $continent        = $source->getContinent();
 
         // Copy the country code
         if ($code !== $destination->getCode()) {
@@ -87,6 +88,11 @@ class CountryRepository extends EntityRepository implements CountryRepositoryInt
         // Copy the phone prefix
         if ($phonePrefix !== $destination->getPhonePrefix()) {
             $destination->setPhonePrefix($phonePrefix);
+        }
+        
+         // Copy the continent
+        if ($continent !== $destination->getContinent()) {
+            $destination->setContinent($continent);
         }
     }
 
